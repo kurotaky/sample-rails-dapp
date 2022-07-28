2482f7e08e5450061d0dc90501bbf0b11f3b7c50
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2022-07-28 10:14:03 +0900

    Greeter

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity >=0.8.0 <0.9.0;
 
-contract Greet {
+contract Greeter {
     string name;
 
     function setName(string calldata _name) public {
